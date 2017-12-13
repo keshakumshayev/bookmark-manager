fd40f89f5f885695a18b1db321f721692b0c1d2d
kkumshayev <kumshkesh@gmail.com>
2017-12-13 17:07:04 +0000

    failing test for tagging a link with one tag

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -26,3 +26,18 @@ feature 'creating links' do
     end
   end
 end
+
+feature 'Adding tags' do
+
+  scenario 'I can add a single tag to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags',  with: 'education'
+
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education')
+  end
+
+end
