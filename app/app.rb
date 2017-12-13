de89bdfa26143ddeb7605cf8d679eb78446ba17c
kkumshayev <kumshkesh@gmail.com>
2017-12-13 17:17:22 +0000

    now able to tag links

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,6 @@
 # ENV["RACK_ENV"] ||= "development"
 
-require_relative 'models/link'
+require_relative 'data_mapper_setup'
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
@@ -21,8 +21,12 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
-    redirect '/links'
+    link = Link.new(url: params[:url],     # 1. Create a link
+                  title: params[:title])
+    tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
+    link.tags << tag                       # 3. Adding the tag to the link's DataMapper collection.
+    link.save                              # 4. Saving the link.
+    redirect to('/links')
   end
 
   run! if app_file == $0
