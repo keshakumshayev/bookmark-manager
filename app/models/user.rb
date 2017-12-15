af2aff66c116b27ee7bdf267e39a49be8537b466
kkumshayev <kumshkesh@gmail.com>
2017-12-15 16:31:26 +0000

    failing heroku, passing everything else

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+require './app/data_mapper_setup'
+require 'bcrypt'
+
+class User
+  include DataMapper::Resource
+
+  has n, :links, through: Resource
+
+  property :id, Serial
+  property :email, String
+  property :password_digest, Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
